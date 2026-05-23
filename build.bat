25d19d6a693e67e980fbf01adafd64340f8830db
Hennadii Kolesnikov <74913520+geggabyte@users.noreply.github.com>
2026-05-23 22:55:59 +0200

    Building Updated.

--- build.bat
+++ build.bat
@@ -1,17 +1,22 @@
 cls
 @echo off
+
+echo.
+echo Cleaning bin directory (keeping config.json)...
+for %%f in (.\bin\*) do if /i not "%%~nxf"=="config.json" del "%%f"
+
 echo Building config.exe...
 python3 -m PyInstaller --onefile --windowed --distpath ./bin --name VeadotubeProxyClientConfig config.py -D -F
 python3 -m PyInstaller --onefile --windowed --distpath ./bin --name VeadotubeProxy proxy.py -D -F
 echo.
 echo Build process completed.
+
 echo Copying python files to bin directory...
 copy ".\client.py" ".\bin\client.py"
 copy ".\config_manager.py" ".\bin\config_manager.py"
 copy ".\client.bat" ".\bin\client.bat"
-REM copy ".\proxy.py" ".\bin\proxy.py"
-REM copy ".\proxy.bat" ".\bin\proxy.bat"
 echo.
+
 echo The executables are located in ./bin/
 echo Removing build artifacts...
 del *.spec
